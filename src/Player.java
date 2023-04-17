0b0146d67bed35dcee7c2c59efe332979f252dd4
ste-spa <steve.spatzek@gmail.com>
2023-04-17 19:30:48 +0200

    player class

---
+++ src/Player.java
@@ -0,0 +1,15 @@
+class Player {
+	private Integer id;
+	private String name;
+	private Boolean roundFinished;
+	private Integer[] activities;
+	private Double cashBalance;
+	private Integer stockQuanitity;
+	private String [] playerType;
+	
+
+	Player() {
+		
+	}
+
+}
