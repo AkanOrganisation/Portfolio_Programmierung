bb9edcf71e51ce5d0774e5e891d2f0fa3f961b9b
Alan Kanjo <alan.kanjo@swan.de>
2023-04-18 21:18:46 +0200

    trying to implement deserialization in productCatalog

--- src/CatalogProduct.java
+++ src/CatalogProduct.java
@@ -21,7 +21,11 @@ public class CatalogProduct {
      * @param recommendedPrice the recommended price of the product
      * @param components the list of components that make up the product
      */
-    public CatalogProduct(int id, String name, double recommendedPrice, ArrayList<Component> components) {
+    @JsonCreator
+    public CatalogProduct(@JsonProperty("id") int id,
+                          @JsonProperty("name") String name,
+                          @JsonProperty("recommendedPrice") double recommendedPrice,
+                          @JsonProperty("components") ArrayList<Component> components) {
         this.id = id;
         this.name = name;
         this.recommendedPrice = recommendedPrice;
@@ -31,17 +35,13 @@ public class CatalogProduct {
         catalog.add(this);
     }
 
-    @JsonCreator
-    public CatalogProduct(@JsonProperty("id") String id,
-                          @JsonProperty("name") String name,
-                          @JsonProperty("recommendedPrice") double price) {
-        this.id = Integer.parseInt(id);
-        this.name = name;
-        this.recommendedPrice = recommendedPrice;
-    }
 
     public static CatalogProduct getProductByName(String productName) {
         return catalog.stream().filter(product -> product.name == productName).findFirst().orElse(null);
+    }
+
+    public static CatalogProduct getProductById(int id) {
+        return catalog.stream().filter(product -> product.id == id).findFirst().orElse(null);
     }
 
     public String getName() {
--- src/Component.java
+++ src/Component.java
@@ -1,11 +1,21 @@
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Component {
     int id;
     CatalogProduct product;
     int quantity;
 
-    Component (CatalogProduct product, int quantity){
+    Component(CatalogProduct product, int quantity) {
         this.id = product.id;
         this.product = product;
+        this.quantity = quantity;
+    }
+
+    @JsonCreator
+    Component(@JsonProperty("id") int id, @JsonProperty("quantity") int quantity) {
+        this.id = id;
+        this.product = CatalogProduct.getProductById(id);
         this.quantity = quantity;
     }
 
