e2c3ca60fa239ff8140753ef16f2421cedc9fb52
Alan Kanjo <alan.kanjo@swan.de>
2023-04-27 10:26:39 +0200

    added History class

---
+++ src/History.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class History {
+    private class ProductRecord {
+        int bought;
+        int sold;
+
+        private ProductRecord(int bought, int sold) {
+            this.bought = bought;
+            this.sold = sold;
+        }
+    }
+
+    private final Map<Integer, Map<CatalogProduct, ProductRecord>> history;
+
+    public History() {
+        history = new HashMap<>();
+    }
+
+    public void addRecord(int round, CatalogProduct product, int bought, int sold) {
+        Map<CatalogProduct, ProductRecord> roundMap = history.computeIfAbsent(round, k -> new HashMap<>());
+        roundMap.put(product, new ProductRecord(bought, sold));
+    }
+
+    public ProductRecord getRecord(int round, CatalogProduct product) {
+        return history.getOrDefault(round, new HashMap<>()).getOrDefault(product, new ProductRecord(0, 0));
+    }
+
+    public int getBought(int round, CatalogProduct product) {
+        return getRecord(round, product).bought;
+    }
+
+    public int getSold(int round, CatalogProduct product) {
+        return getRecord(round, product).sold;
+    }
+}
+
