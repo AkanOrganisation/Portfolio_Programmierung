3a7b8cd9838a5f89fd930185708f62096547618b
ste-spa <steve.spatzek@gmail.com>
2023-04-27 09:52:48 +0200

    rewrite the javadoc of Activity

--- src/Activity.java
+++ src/Activity.java
@@ -1,9 +1,55 @@
+
+/**
+
+ This class represents an Activity that a Player can execute in the game.
+ It implements the Buy, Sell, Build, and Consume interfaces.
+ */
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
+import java.util.Random;
 import java.util.Random;
 
 public class Activity implements Buy, Sell, Build, Consume {
+    /**
+     * The player who is executing the Activity
+     */
+    private final Player player;
+
+    /**
+     * The type of Activity being executed
+     */
+    private final ActivityType type;
+
+    /**
+     * The CatalogProduct associated with the Activity
+     */
+    private final CatalogProduct product;
+
+    /**
+     * The minimum quantity of the CatalogProduct that can be involved in the Activity
+     */
+    private final int minQuantity;
+
+    /**
+     * The maximum quantity of the CatalogProduct that can be involved in the Activity
+     */
+    private final int maxQuantity;
+
+    /**
+     * A boolean representing whether the Activity has finished executing
+     */
+    private boolean finished;
+
+    /**
+     * Constructs an Activity with the specified parameters.
+     *
+     * @param player      the player executing the Activity
+     * @param type        the type of Activity being executed
+     * @param product     the CatalogProduct associated with the Activity
+     * @param minQuantity the minimum quantity of the CatalogProduct involved in the Activity
+     * @param maxQuantity the maximum quantity of the CatalogProduct involved in the Activity
+     */
+
     private final Player player;
     private final ActivityType type;
     private final CatalogProduct product;
@@ -21,7 +67,12 @@ public class Activity implements Buy, Sell, Build, Consume {
         this.finished = false;
 
     }
-
+    /**
+     * Executes the Activity, involving a random quantity of the CatalogProduct.
+     * Calls the appropriate method depending on the type of Activity being executed.
+     *
+     * @throws InterruptedException if the Activity is interrupted
+     */
     public void execute() throws InterruptedException {
         int quantity = new Random().nextInt(maxQuantity - minQuantity) + minQuantity;
         switch (type) {
@@ -33,18 +84,47 @@ public class Activity implements Buy, Sell, Build, Consume {
         }
         this.finished = true;
     }
-
+/**
+ * Returns a boolean representing whether the Activity has finished executing.
+ *
+ * @return true if the Activity has finished executing, false otherwise
+ */
+    publ
     public boolean isFinished() {
         return this.finished;
     }
-
+    /**
+     * A nested class representing the data needed to construct an Activity.
+     */
     public static class Data {
 
+        /**
+         * The type of Activity being executed
+         */
         ActivityType type;
-        CatalogProduct product;
-        int minQuantity;
-        int maxQuantity;
 
+        /**
+         * The CatalogProduct associated with the Activity
+         */
+        CatalogProduct product;
+
+        /**
+         * The minimum quantity of the CatalogProduct involved in the Activity
+         */
+        int minQuantity;
+
+        /**
+         * The maximum quantity of the CatalogProduct involved in the Activity
+         */
+        int maxQuantity;
+        /**
+         * Constructs a Data object with the specified parameters.
+         *
+         * @param type        the type of Activity being executed
+         * @param product     the CatalogProduct associated with the Activity
+         * @param minQuantity the minimum quantity of the CatalogProduct involved in the Activity
+         * @param maxQuantity the maximum quantity of the CatalogProduct involved in the Activity
+         */
         @JsonCreator
         Data(@JsonProperty("type") String type,
              @JsonProperty("product") String product,
@@ -73,12 +153,23 @@ public class Activity implements Buy, Sell, Build, Consume {
         }
     }
 
+    /**
+
+     This class represents an enumerated type of activity types that can be performed.
+     The activity types are BUY, SELL, BUILD, and CONSUME.
+     This class also contains a static method to convert a string representation of the activity type to its corresponding enumeration value.
+     */
     public enum ActivityType {
         BUY,
         SELL,
         BUILD,
         CONSUME;
-
+        /**
+         This static method converts a string representation of an activity type to its corresponding enumeration value.
+         @param name a string representation of the activity type to be converted.
+         @return the corresponding enumeration value of the activity type.
+         @throws IllegalArgumentException if the string representation does not match any of the defined activity types.
+         */
         public static ActivityType fromName(String name) {
             for (ActivityType type : ActivityType.values()) {
                 if (type.name().equalsIgnoreCase(name)) {
