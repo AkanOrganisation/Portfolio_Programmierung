a5fa93a9d988a221428c5cfadf1c4b381fc5f960
Alan Kanjo <alan.kanjo@swan.de>
2023-05-05 14:08:22 +0200

    bugfix concurrent modification in Stock

--- src/Player.java
+++ src/Player.java
@@ -11,57 +11,52 @@ import java.util.stream.Collectors;
 
 
 /**
-
- The Player class represents a player in a stock trading game.
- Each player has a unique ID, a name, a type, a stock portfolio, and a balance of money.
+ * The Player class represents a player in a stock trading game.
+ * Each player has a unique ID, a name, a type, a stock portfolio, and a balance of money.
  */
 public class Player {
 
     /**
-     A list of all players in the game.
+     * A list of all players in the game.
      */
     private final static ArrayList<Player> players = new ArrayList<>();
 
     /**
-     The ID of the next player to be created.
+     * The ID of the next player to be created.
      */
     private static int nextId = 0;
     /**
-     The name of the player.
+     * The name of the player.
      */
     private final String name;
     /**
-     The ID of the player.
+     * The ID of the player.
      */
     private final int id;
     /**
-
-     The type of the player.
-     */
-    private Type type;
-    /**
-
-     The stock portfolio of the player.
+     * The stock portfolio of the player.
      */
     private final Stock stock;
     /**
-     The balance of money of the player.
-     */
-    private double money = Double.POSITIVE_INFINITY;
-    /**
-     The list of activities of the player.
+     * The list of activities of the player.
      */
     private final ArrayList<Activity> activities;
-
-
     private final History history;
-
     private final double priceTolerance;
+    /**
+     * The type of the player.
+     */
+    private Type type;
+    /**
+     * The balance of money of the player.
+     */
+    private double money = Double.POSITIVE_INFINITY;
 
     /**
-     Constructs a player with the given name and type.
-     @param name the name of the player
-     @param type the type of the player
+     * Constructs a player with the given name and type.
+     *
+     * @param name the name of the player
+     * @param type the type of the player
      */
     public Player(String name, Type type, ArrayList<Activity.Data> activities, double priceTolerance) {
         this.id = getNextId();
@@ -77,44 +72,46 @@ public class Player {
     }
 
     /**
-     Returns the number of active players currently in the game.
-     @return the number of active players currently in the game
+     * Returns the number of active players currently in the game.
+     *
+     * @return the number of active players currently in the game
      */
     public static int getNumberOfActivePlayers() {
         return players.size();
     }
 
     /**
-
-     Removes a player from the list of all players in the game.
-     @param player the player to be removed
+     * Removes a player from the list of all players in the game.
+     *
+     * @param player the player to be removed
      */
     public static void removePlayer(Player player) {
         players.remove(player);
     }
 
     /**
-
-     Returns the next available ID for a player and increments the ID counter.
-     @return The next available ID for a player.
-     */
-    private synchronized int getNextId() {
-        return nextId++;
-    }
-
-    /**
-     Adds the given player to the list of active players.
-     @param player The player to add to the list of active players.
+     * Adds the given player to the list of active players.
+     *
+     * @param player The player to add to the list of active players.
      */
     private synchronized static void addPlayerToList(Player player) {
         players.add(player);
     }
 
     /**
+     * Returns the next available ID for a player and increments the ID counter.
+     *
+     * @return The next available ID for a player.
+     */
+    private synchronized int getNextId() {
+        return nextId++;
+    }
 
-     Executes a round of activities for the player.
-     The activities are executed in the order of priority.
-     @throws InterruptedException if the thread is interrupted while waiting
+    /**
+     * Executes a round of activities for the player.
+     * The activities are executed in the order of priority.
+     *
+     * @throws InterruptedException if the thread is interrupted while waiting
      */
     public void playRound() throws InterruptedException {
         prioritizeActivities();
@@ -122,9 +119,8 @@ public class Player {
     }
 
     /**
-
-     Defines the priority order for the activities of the player.
-     The implementation is yet to be defined.
+     * Defines the priority order for the activities of the player.
+     * The implementation is yet to be defined.
      */
     public void prioritizeActivities() {
         //TODO:
@@ -132,63 +128,63 @@ public class Player {
     }
 
     /**
-
-     Logs a message to the application log.
-     @param message the message to be logged
+     * Logs a message to the application log.
+     *
+     * @param message the message to be logged
      */
     public void log(String message, Log.Level level) {
         Log.getInstance().addMessage(message, level);
     }
 
     /**
-
-     Returns the name of the player.
-     @return the name of the player
+     * Returns the name of the player.
+     *
+     * @return the name of the player
      */
     public String getName() {
         return name;
     }
 
     /**
-
-     Returns the type of the player.
-     @return the type of the player
+     * Returns the type of the player.
+     *
+     * @return the type of the player
      */
     public Type getType() {
         return type;
     }
 
     /**
-
-     Returns the stock portfolio of the player.
-     @return the stock portfolio of the player
+     * Returns the stock portfolio of the player.
+     *
+     * @return the stock portfolio of the player
      */
     public Stock getStock() {
         return stock;
     }
 
     /**
-
-     Returns the balance of money of the player.
-     @return the balance of money of the player
+     * Returns the balance of money of the player.
+     *
+     * @return the balance of money of the player
      */
     public double getMoney() {
         return money;
     }
 
     /**
-
-     Adds the given amount of money to the player's account.
-     @param amount the amount of money to be added
+     * Adds the given amount of money to the player's account.
+     *
+     * @param amount the amount of money to be added
      */
     public void addMoney(double amount) {
         this.money += amount;
     }
 
     /**
-
-     Removes the given amount of money from the player's account.
-     @param amount the amount of money to be removed
+     * Removes the given amount of money from the player's account.
+     *
+     * @param amount the amount of money to be removed
      */
     public void removeMoney(double amount) {
         this.money -= amount;
@@ -208,10 +204,35 @@ public class Player {
     }
 
     /**
+     * The Type enum represents the possible types of players in the game.
+     * <p>
+     * The available types are MANUFACTURER, CONSUMER and SUPPLIER.
+     */
+    public enum Type {
+        MANUFACTURER, CONSUMER, SUPPLIER;
 
-     The Controller class represents a controller for a player.
+        /**
+         * Returns the Type instance that corresponds to the given type name.
+         * The name can be provided in any case, as the comparison is case-insensitive.
+         *
+         * @param name a String with the name of the type to be retrieved.
+         * @return the Type instance that corresponds to the given type name.
+         * @throws IllegalArgumentException if the given type name does not match any available type.
+         */
+        public static Type fromName(String name) {
+            for (Type type : Type.values()) {
+                if (type.name().equalsIgnoreCase(name)) {
+                    return type;
+                }
+            }
+            throw new IllegalArgumentException("Invalid player type name: " + name);
+        }
+    }
 
-     It defines the set of activities and their priorities for the player.
+    /**
+     * The Controller class represents a controller for a player.
+     * <p>
+     * It defines the set of activities and their priorities for the player.
      */
     public static class Controller implements Runnable {
 
@@ -223,11 +244,11 @@ public class Player {
         private double priceTolerance;
 
         /**
-
-         Constructs a new Controller object.
-         @param name the name of the controller
-         @param type the type of the controller
-         @param activities the list of activities for the controller
+         * Constructs a new Controller object.
+         *
+         * @param name       the name of the controller
+         * @param type       the type of the controller
+         * @param activities the list of activities for the controller
          */
         @JsonCreator
         Controller(@JsonProperty("name") String name, @JsonProperty("type") String type, @JsonProperty("activities") ArrayList<Activity.Data> activities, @JsonProperty("priceTolerance") double priceTolerance) {
@@ -239,10 +260,11 @@ public class Player {
         }
 
         // Read JSON data from file
-        /**
 
-         Loads the list of controllers from a JSON file.
-         @param filePath the path to the JSON file
+        /**
+         * Loads the list of controllers from a JSON file.
+         *
+         * @param filePath the path to the JSON file
          */
         // Read JSON data from file
         public static void loadFromJsonFile(String filePath) throws LoadError {
@@ -262,27 +284,27 @@ public class Player {
         }
 
         /**
-
-         Returns the number of loaded controllers.
-         @return the number of loaded controllers
+         * Returns the number of loaded controllers.
+         *
+         * @return the number of loaded controllers
          */
         public static int getNumberOfPlayers() {
             return controllers.size();
         }
 
         /**
-
-         Returns the list of loaded controllers.
-         @return the list of loaded controllers
+         * Returns the list of loaded controllers.
+         *
+         * @return the list of loaded controllers
          */
         public static ArrayList<Controller> getPlayersControllers() {
             return controllers;
         }
 
         /**
-
-         Returns the list of loaded controllers.
-         @return the list of loaded controllers
+         * Returns the list of loaded controllers.
+         *
+         * @return the list of loaded controllers
          */
         @Override
         public void run() {
@@ -333,81 +355,55 @@ public class Player {
         }
 
         /**
-
-         Returns the name of the player.
-         @return the name of the player
+         * Returns the name of the player.
+         *
+         * @return the name of the player
          */
         public String getName() {
             return name;
         }
 
     }
-
-    /**
-
-     The Type enum represents the possible types of players in the game.
-
-     The available types are MANUFACTURER, CONSUMER and SUPPLIER.
-     */
-    public enum Type {
-        MANUFACTURER, CONSUMER, SUPPLIER;
-
-        /**
-
-         Returns the Type instance that corresponds to the given type name.
-         The name can be provided in any case, as the comparison is case-insensitive.
-         @param name a String with the name of the type to be retrieved.
-         @return the Type instance that corresponds to the given type name.
-         @throws IllegalArgumentException if the given type name does not match any available type.
-         */
-        public static Type fromName(String name) {
-            for (Type type : Type.values()) {
-                if (type.name().equalsIgnoreCase(name)) {
-                    return type;
-                }
-            }
-            throw new IllegalArgumentException("Invalid player type name: " + name);
-        }
-    }
 }
 
 /**
-
- The Stock class represents the stock of a player's products.
+ * The Stock class represents the stock of a player's products.
  */
 class Stock {
 
     /**
-
-     A map that associates each catalog product to a list of available products in stock.
+     * A map that associates each catalog product to a list of available products in stock.
      */
     private final Map<CatalogProduct, List<CatalogProduct.Product>> stock = new ConcurrentHashMap<>();
 
     /**
-
-     Removes a given quantity of products of a specified catalog product from the stock.
-     @param catalogProduct the catalog product to remove products from
-     @param quantity the quantity of products to remove
+     * Removes a given quantity of products of a specified catalog product from the stock.
+     *
+     * @param catalogProduct the catalog product to remove products from
+     * @param quantity       the quantity of products to remove
      */
     public int removeProducts(CatalogProduct catalogProduct, int quantity) {
         if (stock.containsKey(catalogProduct)) {
             List<CatalogProduct.Product> products = stock.get(catalogProduct);
-            quantity = Math.min(products.size(), quantity);
-            if (quantity > 0) {
-                products.subList(0, quantity).clear();
-                Log.getInstance().addMessage("Removed %d products of type %s from the stock".formatted(quantity, catalogProduct.getName()), Log.Level.DEBUG);
-                return quantity;
+            // Get lock on the list of products
+            synchronized (products) {
+                quantity = Math.min(products.size(), quantity);
+                if (quantity > 0) {
+                    products.subList(0, quantity).clear();
+                    Log.getInstance().addMessage("Removed %d products of type %s from the stock".formatted(quantity, catalogProduct.getName()), Log.Level.DEBUG);
+                    return quantity;
+                }
             }
         }
         return 0;
     }
 
     /**
-
-     Adds a given quantity of a given product to the stock.
-     If the product already exists in the stock, it adds the new products to the existing list of products.
-     @param catalogProduct The product to add.
-     @param quantity The quantity of the product to add.
+     * Adds a given quantity of a given product to the stock.
+     * If the product already exists in the stock, it adds the new products to the existing list of products.
+     *
+     * @param catalogProduct The product to add.
+     * @param quantity       The quantity of the product to add.
      */
     public void addProducts(CatalogProduct catalogProduct, int quantity) {
         List<CatalogProduct.Product> products = stock.computeIfAbsent(catalogProduct, k -> new ArrayList<>());
@@ -419,19 +415,19 @@ class Stock {
     }
 
     /**
-
-     Returns the list of products in the stock for a given product.
-     @param catalogProduct The product to get the list of products for.
-     @return The list of products in the stock for the given product.
+     * Returns the list of products in the stock for a given product.
+     *
+     * @param catalogProduct The product to get the list of products for.
+     * @return The list of products in the stock for the given product.
      */
     public List<CatalogProduct.Product> getProducts(CatalogProduct catalogProduct) {
         return stock.getOrDefault(catalogProduct, new ArrayList<>());
     }
 
     /**
-
-     Returns a map of product quantities for all products in the stock.
-     @return A map of product quantities for all products in the stock.
+     * Returns a map of product quantities for all products in the stock.
+     *
+     * @return A map of product quantities for all products in the stock.
      */
     public Map<CatalogProduct, Integer> getProductQuantities() {
         Map<CatalogProduct, Integer> productQuantities = new ConcurrentHashMap<>();
