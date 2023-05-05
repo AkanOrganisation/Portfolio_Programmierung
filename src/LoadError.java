60b726855468d3d65b7013764cbfbafa5f7ab841
ste-spa <steve.spatzek@gmail.com>
2023-05-05 09:23:52 +0200

    created the javadoc for LoadError

--- src/LoadError.java
+++ src/LoadError.java
@@ -1,10 +1,24 @@
+/**
+ * The LoadError class is an exception used to indicate that an error occurred while loading data from a file.
+ */
 public class LoadError extends Exception {
+
+    /**
+     * Constructs a new LoadError object with the specified cause and prints the stack trace.
+     *
+     * @param e the cause of the error
+     */
     public LoadError(Exception e) {
         super(e);
         e.printStackTrace();
         System.out.println("Error loading from file");
     }
 
+    /**
+     * Constructs a new LoadError object with the specified detail message.
+     *
+     * @param s the detail message
+     */
     public LoadError(String s) {
         super(s);
         System.out.println(s);
